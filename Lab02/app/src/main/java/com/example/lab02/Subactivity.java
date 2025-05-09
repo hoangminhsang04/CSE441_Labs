5dbb29d58d26ec02e8a3a0b9aedf6718814553e6
Hoang Minh Sang <tpminhhoang2k4@gmail.com>
2025-05-09 17:33:40 +0700

    Lab02

---
+++ Lab02/app/src/main/java/com/example/lab02/Subactivity.java
@@ -0,0 +1,25 @@
+package com.example.lab02;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Subactivity extends AppCompatActivity {
+    Button btnok;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+// TODO Auto-generated method stub
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_subactivity);
+        btnok = findViewById(R.id.btnok);
+        btnok.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+// TODO Auto-generated method stub
+                finish();
+            }
+        });
+    }
+}
